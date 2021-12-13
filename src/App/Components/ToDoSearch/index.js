de62b2edcf09d8f7e4043be6b7e6dc1d552dc7ce
Gustavo Sandoval <gustavosandoval@Red-Ghost.local>
2021-12-13 15:04:13 -0600

    delete_console

--- src/App/Components/ToDoSearch/index.js
+++ src/App/Components/ToDoSearch/index.js
@@ -5,7 +5,6 @@ import { ToDoContext } from '.././../ToDoContext';
 function ToDoSearch(){
     const {searchValue,setSearchValue}=React.useContext(ToDoContext);
     const onSearchValueChange=(event)=>{
-        console.log(event.target.value);
         setSearchValue(event.target.value);
     };
     return(
